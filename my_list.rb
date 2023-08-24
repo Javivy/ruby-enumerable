338e3542b95a08b3848812437b69fd98a19bfc07
Javier Vargas <Javieralejandroortga@gmail.com>
2023-08-24 13:18:39 -0400

    Create MyEnumerable module with any?, all? and filter methods

--- my_list.rb
+++ my_list.rb
@@ -1,3 +1,5 @@
+require './my_enumerable'
+
 class MyList
   include MyEnumerable
   def initialize(*list)
@@ -7,7 +9,9 @@ class MyList
     yield(@list)
   end
 end
+
 list = MyList.new(1, 2, 3, 4, 6, 7, 8, 9, 10)
+
 puts(list.any? { |num| num == 3 })
 puts(list.all? { |e| e == 3 })
 print list.filter(&:even?)
